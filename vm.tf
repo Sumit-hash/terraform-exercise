a0d133193479d98d7d455c8340a8a591db3ee1ae
Sumit-hash <sumitkharbanda0310@gmail.com>
2026-01-18 17:44:10 +0530

    update resource with ssh connection

--- vm.tf
+++ vm.tf
@@ -14,7 +14,7 @@ resource "google_compute_instance" "openwebui" {
   machine_type = "n2-standard-2"
   zone         = "europe-west1-b"  
 
-  tags = ["foo", "bar"]
+  tags = ["ssh"]
 
   boot_disk {
     initialize_params {
@@ -32,11 +32,24 @@ resource "google_compute_instance" "openwebui" {
   }
 
   metadata = {
-    foo = "bar"
+    ssh-keys = "openwebui:${file("~/.ssh/id_rsa.pub")} openwebui"
   }
 
   service_account {
     email = google_service_account.openwebui.email
     scopes = ["cloud-platform"]
   }
+}
+
+resource "google_compute_firewall" "ssh" {
+  name = "ssh-access"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  target_tags = ["ssh"]
+  source_ranges = ["0.0.0.0/0"]
 }
